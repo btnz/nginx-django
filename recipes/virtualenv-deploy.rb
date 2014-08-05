4e0061ca86b3d979b0a625bd3d1365666a5fcc79
Bevan Thomson <bevan@bevanthomson.net>
2014-08-05 22:13:03 +0100

    python-setup

--- recipes/virtualenv-deploy.rb
+++ recipes/virtualenv-deploy.rb
@@ -7,7 +7,7 @@
 # All rights reserved - Do Not Redistribute
 #
 
-include_recipe "python"
+include_recipe "python-setup"
 
 node[:deploy].each do | application, deploy|
   virtualenv_path = node[:virtualenv_path]
