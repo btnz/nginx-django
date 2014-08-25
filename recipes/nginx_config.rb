41030bd40b3f3e9e7f45c4ef8788eab1b60cb090
Bevan Thomson <bevan@bevanthomson.net>
2014-08-25 21:51:01 +0100

    moved nginx install to AWS

--- recipes/nginx_config.rb
+++ recipes/nginx_config.rb
@@ -7,7 +7,7 @@
 # All rights reserved - Do Not Redistribute
 #
 
-include_recipe "nginx::default"
+#include_recipe "nginx::default"
 
 file "#{node[:nginx][:dir]}/sites-enabled/default" do
   action :delete
